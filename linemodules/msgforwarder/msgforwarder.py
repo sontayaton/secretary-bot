e277d3c51baab4a4422d4dcefba1792ddd1d57c3
sontaya.sri@gmail.com <sontaya.sri@gmail.com>
2018-10-01 15:15:31 +0700

    keyword into code

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -11,15 +11,21 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,SourceUse
 
 
 
-mention_id = os.getenv('LINE_MODFORWARD_ID', None)    
-with open('data/mention.json', 'r') as f:
-    mention_dict = json.load(f)
+mention_id = os.getenv('LINE_MODFORWARD_ID', None) 
 
+####### Read keyword from json ############   
+#with open('data/mention.json', 'r') as f:
+#    mention_dict = json.load(f)
+##########################################
+
+mention_dict = ["ton","tonson","ต้น","ต้นสน"]
 
 def forwardMsgToUser(line_bot_api,event):
+
+	if event.message.type 
 	text = event.message.text
 
-	for word in mention_dict["keywords"]:
+	for word in mention_dict:
 		if word in text:
 			try:
 			    line_bot_api.push_message(mention_id, TextSendMessage(text=text))
