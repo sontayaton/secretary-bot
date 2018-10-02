c362c1f7fba5635756e2760c9434c00b0d12af1c
sontaya.sri@gmail.com <sontaya.sri@gmail.com>
2018-10-02 12:06:13 +0700

    update

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -30,7 +30,7 @@ def forwardMsgToUser(line_bot_api,event):
 			try:
 		
 				icon = IconComponent(url='test',size='md')
-				print(str(icon))
+				#print(str(icon))
 				# Compose message for forwarding to user
 				#if isinstance(event.source, SourceGroup):
 					#group = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
