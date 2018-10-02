bf59dfeacfbce65949c9d098c1b179074cd5c82c
sontaya.sri@gmail.com <sontaya.sri@gmail.com>
2018-10-02 11:14:11 +0700

    update

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -36,10 +36,10 @@ def forwardMsgToUser(line_bot_api,event):
 					#group = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
 					#profile = line_bot_api.get_profile(event.source.user_id)
 					#flexMsg['header']['contents'][0]['url'] = group.picture_url
-				print(json.dump(flexMsg))
+				
 					
 				# Push message that contain keyword to User,Group,Room 
-			    #line_bot_api.push_message(mention_id, FlexSendMessage(contents=flexMsg))
+			    line_bot_api.push_message(mention_id, FlexSendMessage(contents=flexMsg))
 
 			    # Reply "Read" message to Chanel if contain keyword occur 
 			    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Read'))
