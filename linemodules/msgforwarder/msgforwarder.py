6852425e4647bbdc0376274234e6736a6685570e
sontaya.sri@gmail.com <sontaya.sri@gmail.com>
2018-10-02 10:48:10 +0700

    update

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -28,10 +28,19 @@ def forwardMsgToUser(line_bot_api,event):
 	for word in keywords:
 		if word in text:
 			try:
-				flexMsg = json.load('models/flexmessage_group.json')
-				print(json.dump(flexMsg))
+				with open('models/flexmessage_group.json', 'r') as f:
+					flexMsg = json.load(f)
+				
+				# Compose message for forwarding to user
+				if event.source.type == 'group':
+					flexMsg = json.load('models/flexmessage_group.json')
+					group = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
+					profile = line_bot_api.get_profile(event.source.user_id)
+					flexMsg['header']['contents'][0]['url'] = group.picture_url
+					print(json.dump(flexMsg))
+					
 				# Push message that contain keyword to User,Group,Room 
-			    line_bot_api.push_message(mention_id, FlexSendMessage(contents=flexMsg))
+			    line_bot_api.push_message(mention_id, FlexSendMessage(contents=text))
 
 			    # Reply "Read" message to Chanel if contain keyword occur 
 			    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Read'))
