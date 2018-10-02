2cf920a1e39fb6d3e716544f01ef30ade716153f
sontaya.sri@gmail.com <sontaya.sri@gmail.com>
2018-10-02 11:11:18 +0700

    update

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -32,11 +32,11 @@ def forwardMsgToUser(line_bot_api,event):
 					flexMsg = json.load(f)
 				
 				# Compose message for forwarding to user
-				if isinstance(event.source, SourceGroup):
+				#if isinstance(event.source, SourceGroup):
 					#group = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
 					#profile = line_bot_api.get_profile(event.source.user_id)
 					#flexMsg['header']['contents'][0]['url'] = group.picture_url
-					print(json.dump(flexMsg))
+				print(json.dump(flexMsg))
 					
 				# Push message that contain keyword to User,Group,Room 
 			    #line_bot_api.push_message(mention_id, FlexSendMessage(contents=flexMsg))
