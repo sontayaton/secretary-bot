e7a18f0db2fc1c1ee0f36b462b4dad0262f2fca8
Sontaya Sritorn <sontaya.sri@gmail.com>
2018-10-06 11:20:27 +0700

    Update msgforwarder.py

--- linemodules/msgforwarder/msgforwarder.py
+++ linemodules/msgforwarder/msgforwarder.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import json
 import os
 
+
 from linebot.exceptions import (
     LineBotApiError, InvalidSignatureError
 )
@@ -12,6 +13,7 @@ from linebot.models import (MessageEvent,TextMessage, FlexSendMessage, TextSendM
 
 
 mention_id = os.getenv('LINE_MODFORWARD_ID', None) 
+
 
 ####### Read keyword from json ############   
 #with open('data/mention.json', 'r') as f:
@@ -24,7 +26,7 @@ def forwardMsgToUser(line_bot_api,event):
 
 	# Get message from Line event
 	text = event.message.text
-	print(event.source)
+	# print(event.source)
 	for word in keywords:
 		if word in text:
 			try:
@@ -32,32 +34,58 @@ def forwardMsgToUser(line_bot_api,event):
 				#Compose message for forwarding to user
 				if isinstance(event.source, SourceGroup):
 					profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
+				#### HERO BLOCK ####
+					header_components = list()
+					header_user_icon = IconComponent(url=profile.picture_url,size='3xl')
+					header_recvd_msg = TextComponent(text=profile.display_name,size='md')
+					header_components.append(header_user_icon)
+					header_components.append(header_recvd_msg)
+					header = BoxComponent(contents=header_components,layout='baseline', spacing='md')
+				#### END HERO BLOCK ###
+
+				#### BODY BLOCK ####
+				# Build flex message body as message push 
+					body_components = list()
+					#user_icon = IconComponent(url=profile.picture_url,size='xl')
+					recvd_msg = TextComponent(text=text,size='md',wrap=True)
+					#body_components.append(user_icon)
+					body_components.append(recvd_msg)
+					body = BoxComponent(contents=body_components,layout='baseline')
+				#### END BODY BLOCK ###
+					bubbleContainer = BubbleContainer(header=header,body=body)
+					flex = FlexSendMessage(contents=bubbleContainer,alt_text=word)
+
 				elif isinstance(event.source, SourceUser):
 					profile = line_bot_api.get_profile(event.source.user_id)
-				# 	profile = line_bot_api.get_profile(event.source.user_id)
-				# 	# Build flex message body as message push 
-				# 	body_components = list()
-				# 	user_icon = IconComponent(url=group.picture_url,size='md')
-				# 	recvd_msg = TextComponent(text=text,size='md')
-				# 	body_components.append(user_icon)
-				# 	body_components.append(recvd_msg)
-				# 	body_box = list()
-				# 	body_box.append(BoxComponent(contents=body_components,layout='baseline'))
-				# 	body = BoxComponent(contents=body_box,layout='vertical')
+				#### HERO BLOCK ####
+					header_components = list()
+					header_user_icon = IconComponent(url=profile.picture_url,size='3xl')
+					header_recvd_msg = TextComponent(text=profile.display_name,size='md')
+					header_components.append(header_user_icon)
+					header_components.append(header_recvd_msg)
+					header = BoxComponent(contents=header_components,layout='baseline', spacing='md')
+				#### END HERO BLOCK ###
 
-				# 	push_message_container = list()
-				# 	push_message_container.append(BubbleContainer(body=body))
-				# 	print(push_message_container)
-
+				#### BODY BLOCK ####
+				# Build flex message body as message push 
+					body_components = list()
+					#user_icon = IconComponent(url=profile.picture_url,size='xl')
+					recvd_msg = TextComponent(text=text,size='md',wrap=True)
+					#body_components.append(user_icon)
+					body_components.append(recvd_msg)
+					body = BoxComponent(contents=body_components,layout='baseline')
+				#### END BODY BLOCK ###
+					bubbleContainer = BubbleContainer(header=header,body=body)
+					flex = FlexSendMessage(contents=bubbleContainer,alt_text=word)
 					# Push message that contain keyword to User,Group,Room 
-				#line_bot_api.push_message(mention_id, FlexSendMessage(contents=push_message_container))
-				push_text = profile.display_name + ' > ' + text
-				line_bot_api.push_message(mention_id, TextSendMessage(text=push_text))
+				line_bot_api.push_message(mention_id,flex)
+				# push_text = profile.display_name + ' > ' + text
+				# line_bot_api.push_message(mention_id, TextSendMessage(text=push_text))
 				# Reply "Read" message to Chanel if contain keyword occur 
 				line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Read'))
 				break
 			except LineBotApiError as e:
 				# error handle
-				print("Got exception from LINE Messaging API: %s\n" % e.message)
+				print("Got exception from LINE Messaging API: %s\n" % e)
 	
 			
